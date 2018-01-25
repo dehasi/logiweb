16089cd5570efa5e83d3259733829b27f961c55a
Ravil Galeyev <dehasi@yandex.ru>
2018-01-25 21:55:34 +0100

    Rebuild with gradle

---
+++ src/main/java/dh/logiweb/logiweb/LogiWebApplication.java
@@ -0,0 +1,12 @@
+package dh.logiweb.logiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiWebApplication.class, args);
+	}
+}
